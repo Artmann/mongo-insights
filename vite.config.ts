f0bfa9cd756fce0e42465242a44b7a78e0660383
Christoffer Artmann <artgaard@gmail.com>
2026-04-01 15:01:01 +0200

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   root: 'src/app',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/app')
+    }
+  },
   build: {
     outDir: '../../dist/client',
     emptyOutDir: true
